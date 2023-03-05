8b05533544d369a510ad7f6ffd5944b2ed7305e9
Satwik Prabhu <satwikprabhu@gmail.com>
2023-03-05 19:03:31 +0530

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 export default function Navbar(){
     return(
         <nav>
             <h1><span>M</span>ovie<span>M</span>ania</h1>
             <div className='uldiv'>
                 <ul>
-                    <li className="nav-items"><a href="#">Home</a></li>
-                    <li className="nav-items"><a href="#">About</a></li>
-                    <li className="nav-items"><a href="#">Contact</a></li>
-                    <li className="nav-items"><a href="#">Team</a></li>
+                    <li className="nav-items"><a href="">Home</a></li>
+                    <li className="nav-items"><a href="">About</a></li>
+                    <li className="nav-items"><a href="">Contact</a></li>
+                    <li className="nav-items"><a href="">Team</a></li>
                 </ul>
             </div>
                 <button className='navSearch'>Search</button>
