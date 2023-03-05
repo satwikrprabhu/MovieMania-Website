30fd4108bf7a07373f453a80564a7cf3d29f7076
satwikrprabhu <satwikprabhu@gmail.com>
2023-03-05 12:47:26 +0530

    Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 export default function Navbar(){
     return(
         <nav>
-            
+            <h1><span>M</span>ovie<span>M</span>ania</h1>
+            <div className='uldiv'>
+                <ul>
+                    <li className="nav-items"><a href="">Home</a></li>
+                    <li className="nav-items"><a href="">About</a></li>
+                    <li className="nav-items"><a href="">Contact</a></li>
+                    <li className="nav-items"><a href="">Team</a></li>
+                </ul>
+            </div>
+                <button className='navSearch'>Search</button>
         </nav>
     )
 }
