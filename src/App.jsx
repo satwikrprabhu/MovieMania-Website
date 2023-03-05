049d1c694c78046dcdca386f0711b3bedc3ed309
Satwik Prabhu <satwikprabhu@gmail.com>
2023-03-05 19:03:54 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Navbar from './components/Navbar'
+import CarouselFadeExample from './components/CarouselFadeExample'
+import Upnext from './components/Upnext'
+import Content from './components/Content'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -9,7 +13,10 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <CarouselFadeExample />
+      <Upnext/>
+      <Content/>
+      <Footer/>
     </div>
   )
 }
